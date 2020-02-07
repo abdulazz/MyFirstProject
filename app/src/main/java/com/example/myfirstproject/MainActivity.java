3f1dc5f48a8c911bd9e331491caf09b9a1d0ff2f
Abdul Aziz <abdulazz@users.noreply.github.com>
2020-02-07 09:49:49 +0700

    update pertama kali 1.mengubah main activity

--- app/src/main/java/com/example/myfirstproject/MainActivity.java
+++ app/src/main/java/com/example/myfirstproject/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int a =10;
+
         Button btnPindah = findViewById(R.id.btn);
         btnPindah.setOnClickListener(new View.OnClickListener() {
             @Override
